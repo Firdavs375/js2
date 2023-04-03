d5543370a91b4aa5140c65ab5f338f39b5e76ce5
Firdavs375 <firdavss.3755@gmail.com>
2023-04-03 16:24:58 +0500

    fix

--- index.js
+++ index.js
@@ -1,50 +1,66 @@
-function num(a, b, c) {
-    if (a > b && a > c) {
-        console.log(a, "Eng katta son"); // 1 
-    } else if (b > a && b > c) {
-        console.log(b, "Eng katta son"); // 0
-    } else if (c > a && c > b) {
-        console.log(c, "Eng katta son"); // 0
-    }
-    if (a < b && a < c) {
-        console.log(a, "Eng kichik son"); // 0 
-    } else if (b < a && b < c) {
-        console.log(b, "Eng kichik son"); // 1
-    } else if (c < a && c < b) {
-        console.log(c, "Eng kichik son"); // 0
-    } else {
-        console.log("ikkta son teng bolishi mumkun");
-    }
-}
-num(19, 20, 25)
+// let a = +prompt("birinchi misol: birinchi soni kiriting");
+// let b = +prompt("birinchi misol: ikkiinchi soni kiriting");
+// let c = +prompt("birinchi misol: uchinchi soni kiriting");
+
+// function num(a, b, c) {
+//     let j = Math.max(a, b, c)
+//     alert(j);
+//     let j2 = Math.min(a, b, c);
+//     alert(j2);
+// }
+
+// num(a, b, c);
+
+// function num(a, b, c) {
+
+//     if (a > b && a > c) {
+//         alert(a, "Eng katta son"); // 1
+//     } else if (b > a && b > c) {
+//         alert(b, "Eng katta son"); // 0
+//     } else if (c > a && c > b) {
+//         alert(c, "Eng katta son"); // 0
+//     }
+//     if (a < b && a < c) {
+//         alert(a, "Eng kichik son"); // 0
+//     } else if (b < a && b < c) {
+//         alert(b, "Eng kichik son"); // 1
+//     } else if (c < a && c < b) {
+//         alert(c, "Eng kichik son"); // 0
+//     } else {
+//         alert("ikkta son teng bolishi mumkun");
+//     }
+// }
+// num()
 // num = prompt("birinchi misol: birinchi soni kiriting")
 // num = prompt("birinchi misol: ikkinchi soni kiriting")
 // num = prompt("birinchi misol: uchinchi soni kiriting")
 
 
 
+// let d = +prompt("ikkinchi misol: birinchi sonni kiriting");
+// let e = +prompt("ikkinchi misol: birinchi sonni kiriting");
 
-function num2(a, b) {
-    let c = a / b
-    if (a / b) {
-        console.log(+a + " Niqob " + b + " odamga " + c + " tadan taqsimlanadi.");
-    }
-
-}
-
-num2(20, 10)
+// function num2(d, e) {
+//     let w = d / e
+//     if (d / e) {
+//         alert(+d + " Niqob " + e + " odamga " + w + " tadan taqsimlanadi.");
+//     }
+// }
+// num2(d, e)
 // num2 = prompt("ikkinchi misol: birinchi sonni kiriting")
 // num2 = prompt("ikkinchi misol: ikkinchi sonni kiriting")
 
+const q = +prompt ("uchinchi misol: birinchi sonni kiriting")
+const r = +prompt ("uchinchi misol: ikkinchi sonni kiriting")
 
-function num3(a, b) {
-    if (a <= b || a > b) {
-        console.log(b);
-    } if (b <= a || b > a) {
-        console.log(a);
+function num3(q, r) {
+    if (q <= r || q > r) {
+        alert(r);
+    } if (r <= q || r > q) {
+        alert(q);
     }
 }
 
-num3(15, 17)
+num3(q,r)
 // num3 = prompt("uchinchi misol: birinchi sonni kiriting")
 // num3 = prompt("uchinchi misol: ikkinchi sonni kiriting")
